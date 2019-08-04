9b4d1c1a6a611ae94f9e1709e44a6eea276de810
Saumil <sam10sha@gmail.com>
2019-08-04 10:29:27 -0700

    renamed windows to win_linux

---
+++ win_linux/make.bat
@@ -0,0 +1,122 @@
+@echo off
+
+set INCLUDE_ORIGINAL=%INCLUDE%
+set INCLUDE=%INCLUDE%;%ProgramFiles%\boost\boost_1_70_0
+set LIB_ORIGINAL=%LIB%
+set LIB=%LIB%;%ProgramFiles%\boost_1_70_0\stage\lib
+if [%1] == [main] (
+    call:Main.exe
+) else if [%1] == [clean] (
+    call:clean
+) else (
+    call:error
+)
+set INCLUDE=%INCLUDE_ORIGINAL%
+set INCLUDE_ORIGINAL=
+set LIB=%LIB_ORIGINAL%
+set LIB_ORIGINAL=
+goto done
+
+
+::EXECUTABLES
+:Main.exe
+set obj_dependency_list=obj\driver.obj
+set lib_dependency_list=lib\Network.lib
+call:bin
+if not exist bin\Main.exe (
+	call:Main_exe_obj
+	call:Main_exe_lib
+	link /machine:x64 /libpath:%cd%\resources\boost_1_70_0\lib\x64 /out:bin\Main.exe %obj_dependency_list% %lib_dependency_list%
+)
+set obj_dependency_list=
+set lib_dependency_list=
+goto:eof
+:Main_exe_obj
+set src_dependency_list=src\Sources\driver.cpp
+call:obj
+for %%i in (%src_dependency_list%) do (
+    if not exist obj\%%~ni.obj (
+	    cl /c /EHsc /Od /I%cd%\src\Headers /D_WIN32_WINNT=0x0601 /Foobj\%%~ni.obj %%i
+	)
+)
+set src_dependency_list=
+goto:eof
+:Main_exe_lib
+set lib_dependency_list=lib\Network.lib
+for %%i in (%lib_dependency_list%) do (
+    call:%%i
+)
+set lib_dependency_list=
+goto:eof
+
+
+
+
+:lib\Network.lib
+set obj_dependency_list=obj\HttpByteContent.obj^
+ obj\HttpContent.obj^
+ obj\HttpFileStreamContent.obj^
+ obj\HttpRequestMessage.obj^
+ obj\HttpResponseMessage.obj^
+ obj\HttpStringContent.obj^
+ obj\WebClient.obj
+call:lib
+if not exist lib\Network.lib (
+    call:Network_lib_obj
+	lib /out:lib\Network.lib %obj_dependency_list%
+)
+goto:eof
+:Network_lib_obj
+set src_dependency_list=src\Sources\HttpByteContent.cpp^
+ src\Sources\HttpContent.cpp^
+ src\Sources\HttpFileStreamContent.cpp^
+ src\Sources\HttpRequestMessage.cpp^
+ src\Sources\HttpResponseMessage.cpp^
+ src\Sources\HttpStringContent.cpp^
+ src\Sources\WebClient.cpp
+call:obj
+for %%i in (%src_dependency_list%) do (
+    if not exist obj\%%~ni.obj (
+	    cl /c /EHsc /Od /I%cd%\src\Headers /D_WIN32_WINNT=0x0601 /Foobj\%%~ni.obj %%i
+	)
+)
+set src_dependency_list=
+goto:eof
+
+
+
+
+::DIRECTORIES
+:bin
+if not exist bin (
+    md bin
+)
+goto:eof
+:lib
+if not exist lib (
+	md lib
+)
+goto:eof
+:obj
+if not exist obj (
+    md obj
+)
+goto:eof
+
+
+
+
+::UTILITIES
+:clean
+set del_list=bin lib obj
+for %%i in (%del_list%) do (
+    if exist %%i (
+	    del /q %%i\*
+	)
+)
+set del_list=
+goto:eof
+:error
+echo Invalid arguments
+goto:eof
+:done
---
+++ win_linux/src/Sources/HttpRequestMessage.cpp
@@ -0,0 +1,266 @@
+#include "stdafx.h"
+#include "HttpRequestMessage.h"
+
+
+// Public construction
+Network::HttpRequestMessage::HttpRequestMessage() :
+    mDefaultProtocol("http"),
+    mBody(NULL)
+{
+	
+}
+Network::HttpRequestMessage::HttpRequestMessage(const std::string& Method, const std::string& URL) :
+    mMethod(Method),
+    mURL(URL),
+    mDefaultProtocol("http"),
+    mBody(NULL)
+{
+    DecodeURL();
+}
+Network::HttpRequestMessage::~HttpRequestMessage()
+{
+    if(mBody)
+    {
+        delete mBody;
+    }
+}
+
+// Public methods
+std::string Network::HttpRequestMessage::GetMethod() const
+{
+    return mMethod;
+}
+std::string Network::HttpRequestMessage::GetURL() const
+{
+    return mURL;
+}
+std::string Network::HttpRequestMessage::GetProtocol() const
+{
+    return mProtocol;
+}
+std::string Network::HttpRequestMessage::GetServerHostName() const
+{
+    return mServerHostName;
+}
+std::string Network::HttpRequestMessage::GetServerIPAddress() const
+{
+    return mServerIPAddress;
+}
+long Network::HttpRequestMessage::GetPort() const
+{
+    return mPort;
+}
+std::string Network::HttpRequestMessage::GetQueryPath() const
+{
+    return mQueryPath;
+}
+std::string Network::HttpRequestMessage::GetRequestHeader(const std::string& Key) const
+{
+	std::string HeaderValue;
+	std::map<const std::string, std::string>::const_iterator HeaderIndex = mHeaders.find(Key);
+	if(HeaderIndex != mHeaders.end())
+	{
+		HeaderValue = HeaderIndex->second;
+	}
+	return HeaderValue;
+}
+std::string Network::HttpRequestMessage::GetAllRequestHeaders() const
+{
+    std::string Header;
+    Header += GetMethod() + " " + GetQueryPath() + " HTTP/1.1\r\n";
+    Header += "Host: " + GetServerHostName() + "\r\n";
+    for(std::map<const std::string, std::string>::const_iterator HeaderEntry = mHeaders.begin();
+        HeaderEntry != mHeaders.end();
+        HeaderEntry++)
+    {
+        Header += HeaderEntry->first + ": " + HeaderEntry->second + "\r\n";
+    }
+    return Header;
+}
+std::string Network::HttpRequestMessage::GetRequestBodyString() const
+{
+    const size_t BufSize = 1 << 12; // 4 KB
+    std::istream* ContentStream = NULL;
+    size_t ContentLength = 0;
+    char* Buffer = NULL;
+    std::string RequestBody;
+    
+    if(mBody)
+    {
+        ContentStream = mBody->GetContent();
+        ContentLength = mBody->GetContentLength();
+        
+        if(ContentLength >= BufSize - 1)
+        {
+            Buffer = new char[BufSize];
+            while(ContentStream->eof() == false)
+            {
+                std::memset(Buffer, 0, BufSize);
+                ContentStream->read(Buffer, BufSize - 1);
+                RequestBody += Buffer;
+            }
+        }
+        else
+        {
+            Buffer = new char[ContentLength + 1];
+            std::memset(Buffer, 0, ContentLength + 1);
+			ContentStream->read(Buffer, (std::streamsize)ContentLength);
+            RequestBody += Buffer;
+        }
+        delete[] Buffer;
+		if(RequestBody.length() > 0)
+		{
+			RequestBody += "\r\n";
+		}
+    }
+    return RequestBody;
+}
+void Network::HttpRequestMessage::GetRequestBodyStream(Network::IStreamWrap& Stream) const
+{
+	mBody->GetContent(Stream);
+}
+std::string Network::HttpRequestMessage::GetRequest() const
+{
+    std::string Request;
+    std::string RequestBody = GetRequestBodyString();
+    
+    Request += GetAllRequestHeaders();
+    Request += "\r\n";
+    if(RequestBody.length() > 0)
+    {
+        Request += RequestBody;
+		Request + "\r\n";
+    }
+    return Request;
+}
+
+
+void Network::HttpRequestMessage::SetMethod(const std::string& Method)
+{
+    mMethod = Method;
+}
+void Network::HttpRequestMessage::SetURL(const std::string& URL)
+{
+    mURL = URL;
+    DecodeURL();
+}
+void Network::HttpRequestMessage::SetHeader(const std::string& Key, const std::string& Value)
+{
+    mHeaders[Key] = Value;
+}
+void Network::HttpRequestMessage::SetByteContent(const HttpByteContent* const Content)
+{
+    mBody = new HttpByteContent(*Content);
+}
+void Network::HttpRequestMessage::SetByteContent(const HttpByteContent& Content)
+{
+    mBody = new HttpByteContent(Content);
+}
+void Network::HttpRequestMessage::SetStringContent(const HttpStringContent* const Content)
+{
+    mBody = new HttpStringContent(*Content);
+}
+void Network::HttpRequestMessage::SetStringContent(const HttpStringContent& Content)
+{
+    mBody = new HttpStringContent(Content);
+}
+void Network::HttpRequestMessage::SetStreamContent(const HttpFileStreamContent* const Content)
+{
+    mBody = new HttpFileStreamContent(*Content);
+}
+void Network::HttpRequestMessage::SetStreamContent(const HttpFileStreamContent& Content)
+{
+    mBody = new HttpFileStreamContent(Content);
+}
+
+
+
+
+
+// Private methods
+void Network::HttpRequestMessage::DecodeURL()
+{
+    const std::string ProtocolDelimiter("://");
+    const std::string QueryPathDelimiter("/");
+    const std::string PortDelimiter(":");
+    size_t DelimiterIndex;
+    std::string URLCopy(mURL);
+    mProtocol.clear();
+    mServerHostName.clear();
+    mQueryPath.clear();
+    
+    DelimiterIndex = URLCopy.find(ProtocolDelimiter);
+    if(DelimiterIndex != std::string::npos)
+    {
+        mProtocol = URLCopy.substr(0, DelimiterIndex);
+        URLCopy = URLCopy.substr(DelimiterIndex + ProtocolDelimiter.size());
+    }
+    else
+    {
+        mProtocol = mDefaultProtocol;
+    }
+    DelimiterIndex = URLCopy.find(QueryPathDelimiter);
+    if(DelimiterIndex != std::string::npos)
+    {
+        mQueryPath = URLCopy.substr(DelimiterIndex);
+        URLCopy = URLCopy.substr(0, DelimiterIndex);
+    }
+    else
+    {
+        mQueryPath = QueryPathDelimiter;
+    }
+    DelimiterIndex = URLCopy.find(PortDelimiter);
+    if(DelimiterIndex != std::string::npos)
+    {
+        mServerHostName = URLCopy.substr(0, DelimiterIndex);
+        URLCopy = URLCopy.substr(DelimiterIndex + PortDelimiter.size());
+        mPort = strtol(URLCopy.c_str(), NULL, 10);
+    }
+    else
+    {
+        mServerHostName = URLCopy;
+        mPort = -1;
+    }
+    SetRemoteServerIPAddr();
+}
+void Network::HttpRequestMessage::SetRemoteServerIPAddr()
+{
+    std::vector<IPEndPoint> IPAddrs;
+    ResolveIPAddrs(IPAddrs);
+    if(IPAddrs.size() > 0)
+    {
+        IPEndPoint& SelectedEndPoint = IPAddrs.back();
+        mServerIPAddress = SelectedEndPoint.mIPAddr;
+        if(mPort < 0)
+        {
+            mPort = SelectedEndPoint.mPort;
+        }
+    }
+}
+bool Network::HttpRequestMessage::ResolveIPAddrs(std::vector<Network::HttpRequestMessage::IPEndPoint>& IPAddrs)
+{
+    bool IPAddrsResolved = false;
+    boost::asio::io_service IOService;
+    boost::asio::ip::tcp::resolver Resolver(IOService);
+    boost::asio::ip::tcp::resolver::query Query(mServerHostName, mProtocol);
+    try
+    {
+        for(boost::asio::ip::tcp::resolver::iterator Iterator = Resolver.resolve(Query);
+            Iterator != boost::asio::ip::tcp::resolver::iterator();
+            Iterator++)
+        {
+            boost::asio::ip::tcp::endpoint EndPt = *Iterator;
+            IPAddrs.push_back(
+                IPEndPoint(
+                    EndPt.address().to_string(),
+                    EndPt.port()
+                )
+            );
+        }
+        IPAddrsResolved = true;
+    }
+    catch(int error) { }
+    return IPAddrsResolved;
+}
+
+
