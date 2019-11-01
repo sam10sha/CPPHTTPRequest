fe61b88e5d4540447cd3fabdfb21f35624cb5c0c
sam10sha <sam10sha@users.noreply.github.com>
2019-11-01 14:32:02 -0700

    change buildexpdef to support c style symbol names

--- win_linux/buildexpdef.bat
+++ win_linux/buildexpdef.bat
@@ -1,25 +1,62 @@
 @echo off
 
-if [%3] == [] (
+if [%1] == [cpp] (
+    goto process_cpp
+) else if [%1] == [c] (
+    goto process_c
+) else (
+    echo no language specified
+    goto invalid_language
+)
+goto done
+
+:process_cpp
+if [%4] == [] (
     goto error
 )
-echo EXPORTS > %3
-for %%a in (%2\*.obj) do (
-    dumpbin /out:%2\%%~na.exp /symbols %%a
-    for /f %%b in (%1) do (
-        for /f "tokens=6,8" %%c in ('findstr "%%b(" %2\%%~na.exp') do (
+if exist %4 (
+    findstr "EXPORTS" %4 >> nul && (
+    echo EXPORTS > %4
+    )
+) else (
+    echo EXPORTS > %4
+)
+for %%a in (%3\*.obj) do (
+    dumpbin /out:%3\%%~na.exp /symbols %%a
+    for /f %%b in (%2) do (
+        for /f "tokens=6,8" %%c in ('findstr "%%b(" %3\%%~na.exp') do (
             if [%%c] == [External] (
-                findstr "%%d" %3 > nul || (
-                    echo %%d >> %3
+                findstr "%%d" %4 > nul || (
+                    echo %%d >> %4
                 )
             )
         )
     )
-    del %2\%%~na.exp
+    del %3\%%~na.exp
 )
 goto done
 
+:process_c
+if [%3] == [] (
+    goto error
+)
+if exist %3 (
+    findstr "EXPORTS" %3 && (
+        echo EXPORTS > %3
+    )
+) else (
+    echo EXPORTS > %3
+)
+for /f %%b in (%2) do (
+    echo %%b >> %3
+)
+goto done
+
+:invalid_language
+echo Invalid language specification
+
 :error
-echo Usage: %0 [input_symbol_file_name] [obj_dir] [output_export_def_file_name]
+echo Usage: %0 c [input_symbol_file_name] [output_export_def_file_name]
+echo Usage: %0 cpp [input_symbol_file_name] [obj_dir] [output_export_def_file_name]
 
 :done
